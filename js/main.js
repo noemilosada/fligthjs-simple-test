477e99127b348cadb6871ebc3da60188001a337b
Noemi <noemi@mendeley.com>
2015-01-19 17:06:03 +0000

    FlightJS: Simple example hide and show

--- js/main.js
+++ js/main.js
@@ -6,7 +6,57 @@
  */
 
 jQuery(function($) {
-  'use strict';
-  
-  // hello world code goes here
+    'use strict';
+
+    /**
+     * Constructor first component
+     * 
+     * @method helloWord
+     * @return {Void}
+     */
+    var helloWorld = flight.component(function() {
+        var msg = "Hello World!";
+
+        // Click event handler
+        this.onClick = function(event) {
+            event.stopPropagation();
+            this.$node.text(msg);
+        };
+
+        // Initialize
+        this.after("initialize", function() {
+            this.on('click', this.onClick);
+        });
+    });
+
+    /**
+     * Constructor hide and show component
+     * 
+     * @method hideShow
+     * @return {Void}
+     */
+    var hideShow = flight.component( function() {
+        // Attributes
+        this.defaultAttrs({
+            spanHideShow: 'span'
+        });
+
+        // Click event to toggle content visibility
+        this.onClick = function() {
+            this.select('spanHideShow').animate({ width: "toggle" }, 500);
+        };
+
+        // Initialize
+        this.after("initialize", function() {
+            this.select('spanHideShow').hide(); // Hide text by default
+            this.on('click', this.onClick);
+        });
+    });
+
+    /**
+     * Attach components to the DOM
+     */
+    helloWorld.attachTo('.hello-world');
+    hideShow.attachTo('.hide-show');
+
 });
