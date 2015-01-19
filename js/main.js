32c436b2bacc4e5ebead0a7c515c9bd6472327ce
Noemi <noemi@mendeley.com>
2015-01-19 12:17:40 +0000

    FlightJS: Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * @name: main.js
+ * @author: Noemi Losada <noemi@mendeley.com>
+ * @date: 19/01/2015
+ * @version: 0.0.1
+ */
+
+jQuery(function($) {
+  'use strict';
+  
+  // hello world code goes here
+});
